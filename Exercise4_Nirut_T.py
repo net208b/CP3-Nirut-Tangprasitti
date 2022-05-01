ca687b6e92762f5d201d9c8da34763d020cf565c
nirut tangprasitti <nirutrps@gmail.com>
2022-05-01 19:19:10 +0700

    first commit

---
+++ Exercise4_Nirut_T.py
@@ -0,0 +1,17 @@
+'''
+FE = Foundation English
+GB = General Business
+ICS = Introduction to Computer Systems
+CP = Computer Programming
+'''
+
+FE = 85.5
+GB = 76
+ICS = 82.5
+CP = 89
+
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems", ICS)
+print("Computer Programming :", CP)
